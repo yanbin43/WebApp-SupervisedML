13b6a79660d1b9b0b13bdfa9423d74209d06fbb2
yyanbin <yanbin43@gmail.com>
2022-06-10 08:16:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import streamlit as st
 
 st.title('Welcome!')
 
-st.markdown('''
-            This is a machine learning demo with **Linear Regression** and **Classification**.
-            <br><br>
-            Developed by Tan Yan Bin
-            <br>
-            Contact me at [LinkedIn](https://www.linkedin.com/in/yyanbin-tan/) | [E-mail](mail.to:yybbb0001@gmail.com)
-            ''',
-            unsafe_allow_html = True)
+content = '''
+          This is a machine learning demo with **Linear Regression** and **Classification**.
+          <br><br>
+          Developed by Tan Yan Bin
+          <br>
+          Contact me at [LinkedIn](https://www.linkedin.com/in/yyanbin-tan/) | [E-mail](mail.to:yybbb0001@gmail.com)
+          '''
+
+st.markdown(content, unsafe_allow_html = True)
